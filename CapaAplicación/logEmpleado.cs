00f1d6aad0bf2e309279f5c85f350bf1525fe24e
Semp2024 <Semp2024@DESKTOP-OEU4KT7>
2024-12-28 23:37:34 -0500

    Implementación de Vista - Emiliano

--- CapaAplicación/logEmpleado.cs
+++ CapaAplicación/logEmpleado.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CapaDatos;
+using CapaEntidades;
 
 namespace CapaAplicación
 {
@@ -19,5 +21,20 @@ namespace CapaAplicación
             }
         }
         #endregion singleton
+
+        #region metodos
+        public entEmpleado Verificar_Inicio_Sesion(String Usuario, String Contrasena)
+        {
+            try
+            {
+                return datEmpleado.Instancia.Verificar_Inicio_Sesion(Usuario, Contrasena);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        #endregion metodos
     }
 }
---
+++ CapaPresentacion/Controllers/IntranetController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CapaEntidades;
+using CapaAplicación;
+
+namespace CapaPresentacion.Controllers
+{
+    public class IntranetController : Controller
+    {
+        [HttpGet]
+        public ActionResult InicioSesion(String msg)
+        {
+            Session["Empleado"] = null;
+            ViewBag.mensaje = msg;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InicioSesion(FormCollection formulario)
+        {
+            try
+            {
+                String Usuario = Convert.ToString(formulario["txtusuario"]);
+                String Contrasena = Convert.ToString(formulario["txtcontrasena"]);
+                entEmpleado e = logEmpleado.Instancia.Verificar_Inicio_Sesion(Usuario, Contrasena);
+                if (e != null)
+                {
+                    Session["Empleado"] = e;
+                    return View("MenuPrincipal");
+                }
+                else
+                {
+                    return RedirectToAction("InicioSesion", "Intranet", new { msg = "Usuario y/o contraseña incorrectos"});
+                }
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("InicioSesion", "Intranet", new { msg = ex.Message });
+            }
+        }
+        public ActionResult MenuPrincipal()
+        {
+            return View();
+        }
+    }
+}
