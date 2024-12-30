06d4bcbc8b1395f51d615318fa1b065a69187e3b
Semp2024 <Semp2024@DESKTOP-ER1TIJ4>
2024-12-29 21:30:20 -0500

    Revert "Actu - Emiliano"
    This reverts commit 39a2e81d2b6f068399c92415b529130326157f7a.

--- CapaAplicación/logEmpleado.cs
+++ CapaAplicación/logEmpleado.cs
@@ -23,19 +23,6 @@ namespace CapaAplicación
         #endregion singleton
 
         #region metodos
-
-        public List<entEmpleado> ListarEmpleado()
-        {
-            try
-            {
-                return datEmpleado.Instancia.ListarEmpleado();
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
         public entEmpleado Verificar_Inicio_Sesion(String Usuario, String Contrasena)
         {
             try
@@ -48,58 +35,6 @@ namespace CapaAplicación
                 throw ex;
             }
         }
-
-        public entEmpleado BuscarEmpleado(int idEmpleado)
-        {
-            try
-            {
-                return datEmpleado.Instancia.BuscarEmpleado(idEmpleado);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-
-        }
-
-        public Boolean InsertarEmpleado(entEmpleado p)
-        {
-            try
-            {
-                return datEmpleado.Instancia.InsertarEmpleado(p);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-
-        }
-
-        public Boolean EditarEmpleado(entEmpleado e)
-        {
-            try
-            {
-                return datEmpleado.Instancia.EditarEmpleado(e);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        public Boolean EliminarEmpleado(int idEmpleado)
-        {
-            try
-            {
-                return datEmpleado.Instancia.EliminarEmpleado(idEmpleado);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-
-        }
-
         #endregion metodos
     }
 }
